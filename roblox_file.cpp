e540f0768771f555f3dda115580a8ea0e6cef169
legoisland21 <wrzochalmikolaj@gmail.com>
2025-08-14 19:03:02 +0200

    Add files via upload

--- roblox_file.cpp
+++ roblox_file.cpp
@@ -3,13 +3,18 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <thread>
+#include <atomic>
+#include <windows.h>
 using namespace std;
+
 string filename;
 bool repeat;
 vector<string> lines;
 string line;
 
 Controller control;
+atomic<bool> running(true);
 
 bool isWindowFocused(HWND hwnd) {
     return GetForegroundWindow() == hwnd;
@@ -20,52 +25,40 @@ void sendBackspace() {
     inputs[0].type = INPUT_KEYBOARD;
     inputs[0].ki.wScan = 0x0E;
     inputs[0].ki.dwFlags = KEYEVENTF_SCANCODE;
-
     inputs[1].type = INPUT_KEYBOARD;
     inputs[1].ki.wScan = 0x0E;
     inputs[1].ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
-
     SendInput(2, inputs, sizeof(INPUT));
 }
 
 void inputSlashScan() {
     INPUT input[2] = {};
-
     input[0].type = INPUT_KEYBOARD;
     input[0].ki.dwFlags = KEYEVENTF_SCANCODE;
     input[0].ki.wScan = 0x35;
-
     input[1].type = INPUT_KEYBOARD;
     input[1].ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
     input[1].ki.wScan = 0x35;
-
     SendInput(2, input, sizeof(INPUT));
 }
 
 void inputEnterScan() {
     INPUT input[2] = {};
-
     input[0].type = INPUT_KEYBOARD;
     input[0].ki.dwFlags = KEYEVENTF_SCANCODE;
     input[0].ki.wScan = 0x1C;
-
     input[1].type = INPUT_KEYBOARD;
     input[1].ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
     input[1].ki.wScan = 0x1C;
-
     SendInput(2, input, sizeof(INPUT));
 }
 
 void sendMessage(const string& text) {
-    cout << "Pressing /" << endl;
     inputSlashScan();
     Sleep(100);
-    cout << "Clearing chat" << endl;
     sendBackspace();
-    cout << "Typing string: " << text << endl;
     control.typeString(text, 0);
     Sleep(10);
-    cout << "Pressing enter (x10)" << endl;
     for(int i = 0; i != 10; i++) { inputEnterScan(); Sleep(10); }
 }
 
@@ -74,18 +67,40 @@ void focusRoblox(HWND hwnd) {
     SetFocus(hwnd);
 }
 
+void hotkeyListener() {
+    RegisterHotKey(NULL, 1, MOD_ALT, 0x51);
+    MSG msg = {};
+    while (running) {
+        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+            if (msg.message == WM_HOTKEY && msg.wParam == 1) {
+                running = false;
+                return;
+            }
+        }
+        this_thread::sleep_for(chrono::milliseconds(50));
+    }
+    UnregisterHotKey(NULL, 1);
+}
+
 int main() {
+    thread hotkeyThread(hotkeyListener);
+
     HWND hwnd = FindWindowA(NULL, "Roblox");
     if (!hwnd) {
         cerr << "Could not find Roblox, is Roblox running?" << endl;
+        running = false;
+        hotkeyThread.join();
         return 1;
     }
+
     cout << "Enter filename to load lines from: ";
     getline(cin, filename);
 
     ifstream file(filename);
     if (!file.is_open()) {
         cerr << "Can't open file: " << filename << endl;
+        running = false;
+        hotkeyThread.join();
         return 1;
     }
 
@@ -103,33 +118,28 @@ int main() {
     cin >> repeat;
     cin.ignore();
 
-    printf("HWND id is %p \n", hwnd);
-    cout << "Setting Roblox as foreground window" << endl;
     SetForegroundWindow(hwnd);
-    cout << "Focusing Roblox" << endl;
     SetFocus(hwnd);
 
     if(repeat) {
-        cout << "Repeating entire file..." << endl;
-        while(true) {
+        while(running) {
             for(const auto& l : lines) {
+                if(!running) break;
                 if(!isWindowFocused(hwnd)) focusRoblox(hwnd);
-                cout << "---------------------------------" << endl;
-                cout << "Sending line: " << l << endl;
                 sendMessage(l);
                 Sleep(delay_ms);
             }
         }
     } else {
         for(const auto& l : lines) {
+            if(!running) break;
             if(!isWindowFocused(hwnd)) focusRoblox(hwnd);
-            cout << "---------------------------------" << endl;
-            cout << "Sending line: " << l << endl;
             sendMessage(l);
             Sleep(delay_ms);
         }
     }
 
-    cout << "Finished sending all lines." << endl;
+    running = false;
+    hotkeyThread.join();
     return 0;
 }
--- roblox_spam.cpp
+++ roblox_spam.cpp
@@ -1,10 +1,33 @@
 #include "autocontroller.h"
+#include <windows.h>
+#include <tlhelp32.h>
 #include <iostream>
 using namespace std;
+
 string text;
 int delay;
 
 Controller control;
+
+void killProcessByName(const wchar_t* processName) {
+    PROCESSENTRY32W pe32;
+    pe32.dwSize = sizeof(PROCESSENTRY32W);
+
+    HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (Process32FirstW(hSnap, &pe32)) {
+        do {
+            if (_wcsicmp(pe32.szExeFile, processName) == 0) {
+                HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, pe32.th32ProcessID);
+                if (hProcess) {
+                    TerminateProcess(hProcess, 0);
+                    CloseHandle(hProcess);
+                    wcout << L"Killed " << processName << L" 🗿🔥\n";
+                }
+            }
+        } while (Process32NextW(hSnap, &pe32));
+    }
+    CloseHandle(hSnap);
+}
 
 bool isWindowFocused(HWND hwnd) {
     return GetForegroundWindow() == hwnd;
@@ -25,7 +48,6 @@ void sendBackspace() {
 
 void inputSlashScan() {
     INPUT input[2] = {};
-
     input[0].type = INPUT_KEYBOARD;
     input[0].ki.dwFlags = KEYEVENTF_SCANCODE;
     input[0].ki.wScan = 0x35;
@@ -39,7 +61,6 @@ void inputSlashScan() {
 
 void inputEnterScan() {
     INPUT input[2] = {};
-
     input[0].type = INPUT_KEYBOARD;
     input[0].ki.dwFlags = KEYEVENTF_SCANCODE;
     input[0].ki.wScan = 0x1C;
@@ -70,28 +91,43 @@ void focusRoblox(HWND hwnd) {
 }
 
 int main() {
+    if (!RegisterHotKey(NULL, 1, MOD_ALT, 0x51)) {
+        cerr << "Failed to register hotkey.\n";
+        return 1;
+    }
+
     HWND hwnd = FindWindowA(NULL, "Roblox");
-    if(hwnd) {
-        cout << "Enter text: ";
-        getline(cin, text);
-        cout << "Enter delay: ";
-        cin >> delay;
-        cout << endl;
-        printf("HWND id is %p \n", hwnd);
-        cout << "Setting Roblox as foreground window" << endl;
-        SetForegroundWindow(hwnd);
-        cout << "Focusing Roblox" << endl;
-        SetFocus(hwnd);
-        cout << "Typing " << text << endl;
-        while(true) {
-            if(!isWindowFocused(hwnd)) focusRoblox(hwnd);
-            cout << "---------------------------------" << endl;
-            sendMessage(text);
-            Sleep(delay);
-        }
-    } else {
+    if (!hwnd) {
         MessageBoxA(NULL, "Could not find Roblox, is Roblox running?", "ERROR", MB_ICONERROR);
         return 1;
     }
+
+    cout << "Enter text: ";
+    getline(cin, text);
+    cout << "Enter delay (ms): ";
+    cin >> delay;
+    cout << endl;
+
+    printf("HWND id is %p \n", hwnd);
+    cout << "Setting Roblox as foreground window" << endl;
+    SetForegroundWindow(hwnd);
+    SetFocus(hwnd);
+    cout << "Typing " << text << endl;
+
+    MSG msg = {0};
+    while (true) {
+        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+            if (msg.message == WM_HOTKEY) {
+                killProcessByName(L"roblox_spammer.exe");
+                return 0;
+            }
+        }
+
+        if(!isWindowFocused(hwnd)) focusRoblox(hwnd);
+        cout << "---------------------------------" << endl;
+        sendMessage(text);
+        Sleep(delay);
+    }
+
     return 0;
 }
